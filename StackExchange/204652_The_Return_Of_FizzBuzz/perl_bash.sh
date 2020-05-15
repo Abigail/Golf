127552be9eba28ee7a108016c2adb58e73587e10
Abigail <abigail@abigail.be>
2020-05-15 22:31:27 +0200

    New challenge

---
+++ StackExchange/204652_The_Return_Of_FizzBuzz/perl_bash.sh
@@ -0,0 +1,1 @@
+eval 'for i in `seq 500`;do echo $((i*2-1))FizzBuzz;done;exit';say$_%3?$_:FizzBuzz for 1..1e3
---
+++ StackExchange/204652_The_Return_Of_FizzBuzz/perl_c.c
@@ -0,0 +1,3 @@
+//;say$_*2-1,Fizzbuzz for 1..500;<<'}';
+main(){for(int i=1;i<1001;i++){i%3?printf("%d",i):printf("FizzBuzz");}
+}
