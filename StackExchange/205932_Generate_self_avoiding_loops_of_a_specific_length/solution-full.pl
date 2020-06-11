c5e7e35810c7776039fb80df061fec30a3e75c5f
Abigail <abigail@abigail.be>
2020-06-11 14:01:55 +0200

    Optimized a few bytes away.

--- StackExchange/205932_Generate_self_avoiding_loops_of_a_specific_length/solution-full.pl
+++ StackExchange/205932_Generate_self_avoiding_loops_of_a_specific_length/solution-full.pl
@@ -11,7 +11,7 @@ no  warnings 'syntax';
 use experimental 'signatures';
 
 my $k = 2 * $_;
-my %m = qw [00 D 01 L 10 R 11 U];
+my @m = ("D", "L", "R", "U");
 
 y/D// == y/U//  &&   # Does $_ contain as many D's as U's ?
 y/R// == y/L//  &&   # Does $_ contain as many R's as L's ?
@@ -37,8 +37,13 @@ say                  # print the results
 for                  # do this for each
 map {
     sprintf ("%0$k" . "b", $_)          # Get the binary representation 
-    =~ s/../$m{$&}/gr                   # And replace pairs of digits
-                                        # with directions
+    =~ s/../'$m[' . "0b$&]"/geer        # And replace pairs of digits
+                                        # with directions; we're using a
+                                        # double eval -- first to turn the
+                                        # replacement part into '$m[0bXX]',
+                                        # with XX the two binary digits from
+                                        # match, then we eval that result to
+                                        # get the right direction.
 } 0 .. 4 ** $_;      # Range of numbers
                                         
 
