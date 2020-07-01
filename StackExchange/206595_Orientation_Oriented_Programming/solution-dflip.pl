3de80966075d34c704e8b80a0b8798bead2926ba
Abigail <abigail@abigail.be>
2020-07-01 17:38:45 +0200

    Fix task.
    The 'google' tasks asks for the lenght of the string, not the reverse.

--- StackExchange/206595_Orientation_Oriented_Programming/solution-dflip.pl
+++ StackExchange/206595_Orientation_Oriented_Programming/solution-dflip.pl
@@ -1,3 +1,3 @@
-$_=<>;/^google$/&&die;say"".reverse;#}oder;_$++yas{
+$_=<>;/^google$/&&die;say length;   #}oder;_$++yas{
 =pod                                           dop=
 say+(1 x<>)!~/^1?$|^(11+)\1+$/;#;"!dlroW ,olleH"yas
--- StackExchange/206595_Orientation_Oriented_Programming/solution.pl
+++ StackExchange/206595_Orientation_Oriented_Programming/solution.pl
@@ -1,3 +1,3 @@
 say"Hello, World!";#;/$+1\)+11(^|$?1^/~!)><x 1(+yas
 =pod                                           dop=
-{say++$_;redo}#;esrever.""yas;eid&&/$elgoog^/;><=_$
+{say++$_;redo}#   ;htgnel yas;eid&&/$elgoog^/;><=_$
