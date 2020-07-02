ea4ad58ca07a604e652a6b44a7eb5300d23e1dfd
Abigail <abigail@abigail.be>
2020-07-03 01:16:27 +0200

    Fourth set of 3 tasks

---
+++ StackExchange/206595_Orientation_Oriented_Programming/program_4.pl
@@ -0,0 +1,13 @@
+# No input:  Print a Tabula Recta!
+# Integer:   Fibonacci function or sequence
+# String:    Covfefify a string
+if(eof()){$_=join"",A..Z,
+A..Y;/.{26}(?{say$&})(?!)/
+;exit}$_=<>;$v=aeiouy;$c=
+"[^$v]";$v="[$v]";if(
+/^($c*$v*($c))$c*($v)/){
+say$1.((({split//,bpcgdtf.
+vgkkgpbsztdvfzs}->{$2}||$2
+).$3)x 2)}else{@x=(0,1);@x
+=($x[1],$x[0]+$x[1])while
+--$_>=0;say$x[0]}
