d62d239ec977e776cc327e2c6a88560d81500195
Abigail <abigail@abigail.be>
2020-07-02 23:43:32 +0200

    Second set of 3 tasks.

--- StackExchange/206595_Orientation_Oriented_Programming/program_2.pl
+++ StackExchange/206595_Orientation_Oriented_Programming/program_2.pl
@@ -1,4 +1,8 @@
-if(eof()){$_="Hello, World!"}else{$_=<>;chomp;if(/\D/){
-s!a([^aA]*)a?!$1=~y[a-zA-Z][A-Za-z]r!xige;}else{if($_){
-$_="cake";
-}else{$_="Congratulations on your new baby! :D"}}};say;
+# No input:  "Hello, World!"
+# Integer:   It's my Birthday :D
+# String:    No A, just CAPS LOCK
+if(eof()){$_="Hello, World!"}else{$_=<>;chomp;if(
+/^-?\d+$/){if($_){$x=1+2*($_<0?1:$_);@x=(' $'x$_,' |'x
+$_,'-'x$x,'~'x$x,'-'x$x);$_=join"\n",grep$_,@x}else{$_=
+"Congratulations on your new baby! :D"}}else{
+s;a([^aA]*)a?;$1=~y,a-zA-Z,A-Za-z,r;ige}};say;
