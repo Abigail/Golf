6c8a9160cde78dd98b64f59ddf50e2704f42d43a
Abigail <abigail@abigail.be>
2020-07-03 01:15:52 +0200

    Reformat program_3 to make it "shorter".

--- StackExchange/206595_Orientation_Oriented_Programming/program_3.pl
+++ StackExchange/206595_Orientation_Oriented_Programming/program_3.pl
@@ -1,7 +1,9 @@
 # No input:  Count up forever
 # Integer:   Is this number a prime?
 # String:    YouTube Comments #1
-if(eof()){{say++$_;redo}}else{$_=<>;chomp;
-if(/\D/){/^.+?(?{say$&})(?!)/;/^(..*).(?{
-say$1})(?!)/}else{$p=$_>1;$n=$_;$p&&=$n%$_
-for 2..$n-2;say$p?1:0}}
+if(eof()){{say++$_;redo}}
+else{$_=<>;chomp;if(/\D/)
+{/^.+?(?{say$&})(?!)/;/^(
+..*).(?{say$1})(?!)/x}else
+{$p=$_>1;$n=$_;$p&&=$n%$_
+for 2..$n-2;say!!$p}}
