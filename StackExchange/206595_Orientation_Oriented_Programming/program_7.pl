7bd7413b2d839d6c153fd2d8bf22ecb26927e096
Abigail <abigail@abigail.be>
2020-07-03 02:45:55 +0200

    Single task for program 7

---
+++ StackExchange/206595_Orientation_Oriented_Programming/program_7.pl
@@ -0,0 +1,9 @@
+# No input:  Sing Happy Birthday to your favourite programming language
+# Integer:   -
+# String:    -
+#             v          v
+$_="Happy Birt"
+."hday to You";
+say;say;say s
+!to!Dear!r=~s
+!You!Perl!r;say
