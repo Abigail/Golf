0dc1dcef92a22ce9071b7b45b519f736bb677554
Abigail <abigail@abigail.be>
2020-07-08 20:53:38 +0200

    A long long time ago, in a galaxy right here: Perl solution.

---
+++ StackExchange/206883_A_long_long_time_ago_in_a_galaxy_right_here/solution.pl
@@ -0,0 +1,3 @@
+#!/opt/perl/bin/perl
+#% Opt: -pl
+@a=((Cenozoic)x67,(Mesozoic)x186,(Paleozoic)x290);@b=((Quaternary)x4,(Neogene)x20,(Paleogene)x43,(Cretaceous)x79,(Jurassic)x56,(Triassic)x51,(Permian)x47,(Carboniferous)x60,(Devonian)x60,(Silurian)x25,(Ordovician)x41,(Cambrian)x57);$_=$a[$_].$".$b[$_]
