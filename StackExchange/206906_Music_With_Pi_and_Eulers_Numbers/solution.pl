e1f477b115a7617ef9c5cba98747a6b7f95d2c74
Abigail <abigail@abigail.be>
2020-07-09 16:20:00 +0200

    Music with pi and Eulers number: Perl solution

---
+++ StackExchange/206906_Music_With_Pi_and_Eulers_Numbers/solution.pl
@@ -0,0 +1,4 @@
+#% Opt: -MMath::BigFloat
+#% Opt: -Mfeature=say
+#% Opt: -pl
+$n=$_;$p=new Math::BigFloat;$p->accuracy(500);$_=$p->bpi;s/..//;s!.!'('.substr(CDEFGABCDE,$&,1).("'"x($&>6)).', '.((substr 7182818284,$&,1)/4).")\n"!eg;/(.+\n){$n}/;$_=$&
