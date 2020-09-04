a8eae037cb1f3713be5c9db3bd9a5fd60226e71d
Abigail <abigail@abigail.be>
2020-09-04 19:49:42 +0200

    No post processing.

--- StackExchange/206906_Music_With_Pi_and_Eulers_Numbers/solution.pl
+++ StackExchange/206906_Music_With_Pi_and_Eulers_Numbers/solution.pl
@@ -1,4 +1,5 @@
 #% Opt: -MMath::BigFloat
 #% Opt: -Mfeature=say
 #% Opt: -pl
+#% Post: none
 $n=$_;$p=new Math::BigFloat;$p->accuracy(500);$_=$p->bpi;s/..//;s!.!'('.substr(CDEFGABCDE,$&,1).("'"x($&>6)).', '.((substr 7182818284,$&,1)/4).")\n"!eg;/(.+\n){$n}/;$_=$&
